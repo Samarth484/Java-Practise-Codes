05b0bd1fea76ec0458c549775cc0b9cfc3a4400c
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-08-07 17:51:18 +0530

    Year refractored, and is generalised to a formula

--- WineProblem.java
+++ WineProblem.java
@@ -3,24 +3,24 @@ package DyamicProgramming;
 public class WineProblem {
 
 	public static void main(String[] args) {
-		int arr[] = { 2,3,5,1,4 };
-	
-		System.out.println(WineProbRecursive(arr, 0, arr.length - 1, 1));
-		System.out.println(WineProbTopDown(arr, 0, arr.length - 1, 1, new int[arr.length][arr.length]));
+		int arr[] = { 2, 3, 5, 1, 4 };
+
+		System.out.println(WineProbRecursive(arr, 0, arr.length - 1));
+		System.out.println(WineProbTopDown(arr, 0, arr.length - 1, new int[arr.length][arr.length]));
 	}
 
-	private static int WineProbRecursive(int arr[], int si, int ei, int year) {
+	private static int WineProbRecursive(int arr[], int si, int ei) {
+
+		int year = arr.length - (ei - si + 1) + 1;
 		if (si == ei) {
 			return arr[si] * year;
 		}
-		int leftCall = WineProbRecursive(arr, si + 1, ei, year + 1);
-		int rightCall = WineProbRecursive(arr, si, ei - 1, year + 1);
+		int leftCall = WineProbRecursive(arr, si + 1, ei);
+		int rightCall = WineProbRecursive(arr, si, ei - 1);
 
 		return Math.max(leftCall + arr[si] * year, rightCall + arr[ei] * year);
 	}
 
-	
-	
 //	the code below will work perfectly fine for very large sized array for eg had arr been like so:
 //	
 //	int arr[] = new int[1000];
@@ -29,14 +29,16 @@ public class WineProblem {
 //	
 //	then in that case WineProbRecursive would have failed but WineProbTopDown will succeed because it follows DP and hence takes care of
 //	overlapping sub-problems!
-	private static int WineProbTopDown(int arr[], int si, int ei, int year, int strg[][]) {
+	private static int WineProbTopDown(int arr[], int si, int ei, int strg[][]) {
+
+		int year = arr.length - (ei - si + 1) + 1;
 		if (si == ei) {
 			return arr[si] * year;
 		}
 		if (strg[si][ei] != 0)
 			return strg[si][ei];
-		int leftCall = WineProbTopDown(arr, si + 1, ei, year + 1, strg);
-		int rightCall = WineProbTopDown(arr, si, ei - 1, year + 1, strg);
+		int leftCall = WineProbTopDown(arr, si + 1, ei, strg);
+		int rightCall = WineProbTopDown(arr, si, ei - 1, strg);
 
 		return strg[si][ei] = Math.max(leftCall + arr[si] * year, rightCall + arr[ei] * year);
 	}
