813ca73f1202f921d62bb1e61026d2b127d468b7
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-08-07 13:40:12 +0530

    DP TD code added- my code!

--- WineProblem.java
+++ WineProblem.java
@@ -3,17 +3,33 @@ package DyamicProgramming;
 public class WineProblem {
 
 	public static void main(String[] args) {
-		int arr[] = { 1,4,2,3 };
-		System.out.println(WineProb(arr, 0, arr.length-1 , 1));
+//		int arr[] = { 2,3,5,1,4 };
+		int arr[] = new int[1000];
+		for (int i = 1; i <= 999; i++)
+			arr[i] = i;
+//		System.out.println(WineProbRecursive(arr, 0, arr.length - 1, 1));
+		System.out.println(WineProbTopDown(arr, 0, arr.length - 1, 1, new int[arr.length][arr.length]));
 	}
 
-	private static int WineProb(int arr[], int si, int ei, int year) {
+	private static int WineProbRecursive(int arr[], int si, int ei, int year) {
 		if (si == ei) {
 			return arr[si] * year;
 		}
-		int leftCall = WineProb(arr, si + 1, ei, year + 1);
-		int rightCall = WineProb(arr, si, ei - 1, year + 1);
+		int leftCall = WineProbRecursive(arr, si + 1, ei, year + 1);
+		int rightCall = WineProbRecursive(arr, si, ei - 1, year + 1);
 
-		return Math.max(leftCall + arr[si]*year, rightCall + arr[ei]*year);
+		return Math.max(leftCall + arr[si] * year, rightCall + arr[ei] * year);
+	}
+
+	private static int WineProbTopDown(int arr[], int si, int ei, int year, int strg[][]) {
+		if (si == ei) {
+			return arr[si] * year;
+		}
+		if (strg[si][ei] != 0)
+			return strg[si][ei];
+		int leftCall = WineProbTopDown(arr, si + 1, ei, year + 1, strg);
+		int rightCall = WineProbTopDown(arr, si, ei - 1, year + 1, strg);
+
+		return strg[si][ei] = Math.max(leftCall + arr[si] * year, rightCall + arr[ei] * year);
 	}
 }
