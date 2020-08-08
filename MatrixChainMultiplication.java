3f308289fa5a9f006dee795363a38b79ddae5146
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-08-09 00:19:45 +0530

    Bottom Up My Code- feeling delighted!

--- MatrixChainMultiplication.java
+++ MatrixChainMultiplication.java
@@ -7,6 +7,8 @@ public class MatrixChainMultiplication {
 
 		System.out.println(MCMRecursiveGCM(arr, 0, arr.length - 1));
 		System.out.println(MCMTopDownMyCode(arr, 0, arr.length - 1, new int[arr.length][arr.length]));
+		System.out.println(MCMBottomUpMyCode(arr));
+
 	}
 
 	private static int MCMRecursiveGCM(int arr[], int si, int ei) {
@@ -31,13 +33,12 @@ public class MatrixChainMultiplication {
 		return min;
 	}
 
-	
 //	The function below will work for array of very large sizes, i.e. had arr been like so:
-	
+
 //	int arr[] = new int[100];
 //	for (int i = 0; i < 100; i++)
 //		arr[i] = i + 1;
-	
+
 //	then too MCMTopDownMyCode function would have worked but MCMRecursiveGCM would have failed on TLE notes in that case!
 	private static int MCMTopDownMyCode(int arr[], int si, int ei, int strg[][]) {
 
@@ -61,4 +62,20 @@ public class MatrixChainMultiplication {
 
 	}
 
+	private static int MCMBottomUpMyCode(int arr[]) {
+		int n = arr.length;
+		int strg[][] = new int[n][n];
+		for (int si = n - 3; si >= 0; si--) {
+			for (int ei = 0; ei < n; ei++) {
+				if (si + 1 < ei) {
+					int total = Integer.MAX_VALUE;
+					for (int k = si + 1; k <= ei - 1; k++) {
+						total = Math.min(total, strg[si][k] + strg[k][ei] + arr[si] * arr[k] * arr[ei]);
+					}
+					strg[si][ei] = total;
+				}
+			}
+		}
+		return strg[0][n - 1];
+	}
 }
