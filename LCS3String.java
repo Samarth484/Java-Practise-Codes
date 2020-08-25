b929e1f0873c5874a3518a2e0c04c84c4acf6294
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-08-25 21:31:58 +0530

    Recursive Brute Force, vidx done, my logic

--- LCS3String.java
+++ LCS3String.java
@@ -3,10 +3,11 @@ package DyamicProgramming;
 public class LCS3String {
 
 	public static void main(String[] args) {
-		String str1 = "abcd1e2";
-		String str2 = "bc12ea";
-		String str3 = "bd1ea";
-		System.out.println(LCS3Str(str1, str2, str3));
+		String s1 = "abcd1e2";
+		String s2 = "bc12ea";
+		String s3 = "bd1ea";
+		System.out.println(LCS3Str(s1, s2, s3));
+		System.out.println(LCS3SrtingsWithVIDX(s1, s2, s3, 0, 0, 0));
 	}
 
 	private static int LCS3Str(String s1, String s2, String s3) {
@@ -22,6 +23,20 @@ public class LCS3String {
 			return LCS3Str(ros1, ros2, ros3) + 1;
 		else
 			return Math.max(Math.max(LCS3Str(ros1, s2, s3), LCS3Str(s1, ros2, s3)), LCS3Str(s1, s2, ros3));
+
+	}
+
+	private static int LCS3SrtingsWithVIDX(String s1, String s2, String s3, int vidx1, int vidx2, int vidx3) {
+		if (vidx1 == s1.length() || vidx2 == s2.length() || vidx3 == s3.length())
+			return 0;
+		if (s1.charAt(vidx1) == s2.charAt(vidx2) && s2.charAt(vidx2) == s3.charAt(vidx3))
+			return LCS3SrtingsWithVIDX(s1, s2, s3, vidx1 + 1, vidx2 + 1, vidx3 + 1) + 1;
+
+		else
+			return Math.max(
+					Math.max(LCS3SrtingsWithVIDX(s1, s2, s3, vidx1 + 1, vidx2, vidx3),
+							LCS3SrtingsWithVIDX(s1, s2, s3, vidx1, vidx2 + 1, vidx3)),
+					LCS3SrtingsWithVIDX(s1, s2, s3, vidx1, vidx2, vidx3 + 1));
 
 	}
 }
