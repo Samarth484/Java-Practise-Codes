2debc3fbd0cb14b5a72eba3a3ba6ab304eeeced5
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-08-25 23:22:08 +0530

    Top Down, self code implemented

--- LCS3String.java
+++ LCS3String.java
@@ -3,11 +3,13 @@ package DyamicProgramming;
 public class LCS3String {
 
 	public static void main(String[] args) {
-		String s1 = "abcd1e2";
-		String s2 = "bc12ea";
-		String s3 = "bd1ea";
+		String s1 = "AGGT12";
+		String s2 = "12TXAYB";
+		String s3 = "12XBA";
 		System.out.println(LCS3Str(s1, s2, s3));
 		System.out.println(LCS3SrtingsWithVIDX(s1, s2, s3, 0, 0, 0));
+		System.out
+				.println(LCS3StringTD(s1, s2, s3, 0, 0, 0, new int[s1.length() + 1][s2.length() + 1][s3.length() + 1]));
 	}
 
 	private static int LCS3Str(String s1, String s2, String s3) {
@@ -37,6 +39,23 @@ public class LCS3String {
 					Math.max(LCS3SrtingsWithVIDX(s1, s2, s3, vidx1 + 1, vidx2, vidx3),
 							LCS3SrtingsWithVIDX(s1, s2, s3, vidx1, vidx2 + 1, vidx3)),
 					LCS3SrtingsWithVIDX(s1, s2, s3, vidx1, vidx2, vidx3 + 1));
+
+	}
+
+	private static int LCS3StringTD(String s1, String s2, String s3, int vidx1, int vidx2, int vidx3, int strg[][][]) {
+
+		if (vidx1 == s1.length() || vidx2 == s2.length() || vidx3 == s3.length())
+			return 0;
+		if (strg[vidx1][vidx2][vidx3] != 0)
+			return strg[vidx1][vidx2][vidx3];
+		if (s1.charAt(vidx1) == s2.charAt(vidx2) && s2.charAt(vidx2) == s3.charAt(vidx3))
+			return strg[vidx1][vidx2][vidx3] = LCS3StringTD(s1, s2, s3, vidx1 + 1, vidx2 + 1, vidx3 + 1, strg) + 1;
+
+		else
+			return strg[vidx1][vidx2][vidx3] = Math.max(
+					Math.max(LCS3StringTD(s1, s2, s3, vidx1 + 1, vidx2, vidx3, strg),
+							LCS3StringTD(s1, s2, s3, vidx1, vidx2 + 1, vidx3, strg)),
+					LCS3StringTD(s1, s2, s3, vidx1, vidx2, vidx3 + 1, strg));
 
 	}
 }
