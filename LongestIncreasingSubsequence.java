d90db7847e4979e0079b839458b5f4d5dd201332
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-08-27 20:40:00 +0530

    Almost same as mine, GCM code added, which is a bit refractored

--- LongestIncreasingSubsequence.java
+++ LongestIncreasingSubsequence.java
@@ -15,6 +15,7 @@ public class LongestIncreasingSubsequence {
 //			arr[i] = scn.nextInt();
 		int arr[] = { 10, 9, 2, 5, 3, 7, 101, 18 };
 		System.out.println(LIS_Quadratic_time(arr));
+		System.out.println(LISBU_GCM(arr));
 	}
 
 	private static int LIS_Quadratic_time(int arr[]) {
@@ -38,6 +39,30 @@ public class LongestIncreasingSubsequence {
 			ans = Math.max(ans, val);
 		}
 		return ans;
+
+	}
+
+	public static int LISBU_GCM(int[] arr) {
+
+		int max = Integer.MIN_VALUE;
+
+		int[] strg = new int[arr.length];
+
+		Arrays.fill(strg, 1);
+
+		for (int i = 0; i < strg.length; i++) {
+
+			for (int j = 0; j < i; j++) {
+
+				if (arr[j] < arr[i]) {
+					strg[i] = Math.max(strg[i], strg[j] + 1);
+
+					max = Math.max(max, strg[i]);
+				}
+			}
+		}
+
+		return max;
 
 	}
 }
